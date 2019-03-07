08ce31bb704d003b4a9ac16b76b5c1364cbb0a3b
Christina Armstrong <armstrongch6@gmail.com>
2019-03-07 12:28:11 -0500

    describing data

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const app = express();
-const sequelize = require('sequelize')
-const html = require('html-template-tag')
-const layout = require("./views/layout");
+const sequelize = require('sequelize');
+const html = require('html-template-tag');
+const layout = require('./views/layout');
 const { db } = require('./models');
 
-db.authenticate().
-then(() => {
+db.authenticate().then(() => {
   console.log('connected to the database');
-})
+});
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
@@ -17,7 +16,7 @@ app.get('/', (req, res) => {
   res.send(layout(''));
 });
 
-const PORT = 1337;
+const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log(`LISTENING ON PORT ${PORT}`);
