749a42edc7008656f0eba74267148ff3d9b28858
Christina Armstrong <armstrongch6@gmail.com>
2019-03-07 12:27:43 -0500

    describing data

--- models/index.js
+++ models/index.js
@@ -1,6 +1,35 @@
 const Sequelize = require('sequelize');
 const db = new Sequelize('postgres://localhost:5432/wikistack');
 
+const Page = db.define('page', {
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  slug: {
+    type: Sequelize.STRING,
+    unique: true,
+  },
+  content: {
+    type: Sequelize.TEXT,
+  },
+  status: {
+    type: Sequelize.ENUM('open', 'closed'),
+  },
+});
+
+const User = db.define('user', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: Sequelize.STRING,
+  },
+});
+
 module.exports = {
-  db
-}
+  db,
+  Page,
+  User,
+};
