9dc663a5fc5e389b1cf557dee9df7932ce35fc9b
Marielle Combier-Kapel <marielle.combierkapel@gmail.com>
2019-03-07 11:54:03 -0500

    connecting to postgres

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const app = express();
+const sequelize = require('sequelize')
+const html = require('html-template-tag')
+const layout = require("./views/layout");
+const { db } = require('./models');
+
+db.authenticate().
+then(() => {
+  console.log('connected to the database');
+})
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send(layout(''));
 });
 
 const PORT = 1337;
